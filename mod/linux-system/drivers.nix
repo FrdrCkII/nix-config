6986bde409b86b0593dd2cabbf00de5722f83cbc
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-14 10:07:26 +0800

    update

--- mod/linux-system/drivers.nix
+++ mod/linux-system/drivers.nix
@@ -1,18 +1,20 @@
 { config, lib, pkgs, cfg, ... }:
-lib.mkMerge [
-  # https://wiki.nixos.org/wiki/AMD_GPU
-  # https://wiki.archlinuxcn.org/wiki/AMDGPU
-  ( lib.optionals (builtins.elem "amd" cfg.opt.drivers) {
-    environment.systemPackages = with pkgs; [
-      lact
-    ];
-    systemd = {
-      packages = with pkgs; [ lact ];
-      services.lactd.wantedBy = ["multi-user.target"];
-    };
-  } )
+{
+  config = lib.mkMerge [
+    # https://wiki.nixos.org/wiki/AMD_GPU
+    # https://wiki.archlinuxcn.org/wiki/AMDGPU
+    ( lib.optionals (builtins.elem "amd" cfg.opt.drivers) {
+      environment.systemPackages = with pkgs; [
+        lact
+      ];
+      systemd = {
+        packages = with pkgs; [ lact ];
+        services.lactd.wantedBy = ["multi-user.target"];
+      };
+    } )
 
-  ( lib.optionals (builtins.elem "nvidia" cfg.opt.drivers) {
-  } )
+    ( lib.optionals (builtins.elem "nvidia" cfg.opt.drivers) {
+    } )
 
-]
+  ]
+}
