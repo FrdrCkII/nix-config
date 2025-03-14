69008be5affe4524d49d9cb8cb3d2ea8a6ff39cc
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-14 09:40:44 +0800

    update

--- mod/linux-system/system.nix
+++ mod/linux-system/system.nix
@@ -12,6 +12,18 @@
 
     ( lib.mkIf (cfg.sys.type == "linux") {
         system-manager.allowAnyDistro = true;
+        # 修复 https://github.com/numtide/system-manager/issues/170
+        systemd.services.system-manager-restart = {
+          enable = true;
+          serviceConfig = {
+            Description = "Restart system-manager after /nix is mounted";
+            Requires = "nix.mount";
+            After = "nix.mount";
+            Type = "oneshot";
+            ExecStart = "/usr/bin/systemctl start system-manager.target";
+          };
+          WantedBy = [ "multi-user.target" ];
+        };
     } )
     
   ];
