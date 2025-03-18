8241da8c6327adf15b90755369243941b8620946
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-18 22:43:26 +0800

    update

--- lib/linux-home-manager.nix
+++ lib/linux-home-manager.nix
@@ -4,7 +4,7 @@
   system = custom-config.system.system;
   extraSpecialArgs = {
     inherit (custom-args) inputs custom-lib;
-    pkgs = {
+    nixpkgs.pkgs = {
       inherit inputs lib custom-lib;
       inherit (custom-config.packages.pkgs) system config overlays;
       inherit (custom-config.packages) pkgs-stable pkgs-unstable nur;
