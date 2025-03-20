3633694a492bb0da4a59a44d11b195f11fada4c5
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-20 16:55:52 +0800

    update

--- mod/nixos-system/system.nix
+++ mod/nixos-system/system.nix
@@ -16,20 +16,19 @@ lib.mkMerge [
       extraGroups = [ "wheel" "networkmanager" ];
     };
     system.stateVersion = if (cfg.opt.system.version != null) then cfg.opt.system.version else "25.05";
-    boot.kernelPackages = if (cfg.opt.system.channel != null) then cfg.opt.system.kernel else cfg.pkgs.linux;
+    boot.kernelPackages = if (cfg.opt.system.channel != null) then cfg.opt.system.kernel else cfg.pkg.pkgs.linuxPackages;
   }
 
-  ( lib.mkIf (cfg.opt.system.version != null) {
-  } )
   ( lib.mkIf (cfg.opt.system.channel != null) {
     system.autoUpgrade.channel = cfg.opt.system.channel;
   } )
-  ( lib.mkIf (cfg.opt.system.kernel != null) {
-  } )
+
   ( lib.mkIf (cfg.opt.users.root.passwd != null) {
     users.users.root.hashedPassword = cfg.opt.users.root.passwd;
   } )
+
   ( lib.mkIf (cfg.opt.users.user.passwd != null) {
     users.users.${cfg.opt.users.user.name}.hashedPassword = cfg.opt.users.user.passwd;
   } )
+  
 ]
