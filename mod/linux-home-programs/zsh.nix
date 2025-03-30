d9476eac9a4f972e9057136228a96e7db95c1834
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-30 19:35:13 +0800

    update

--- mod/linux-home-programs/zsh.nix
+++ mod/linux-home-programs/zsh.nix
@@ -77,6 +77,10 @@
             fd --type d --follow --exclude ".git" . "$1"
           }
         '')
+        (''
+          export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+          export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
+        '')
       ];
     };
     bash = {
