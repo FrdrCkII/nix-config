5f558077116cbe0bda4883e7f6247a9591f6f7c4
FrdrCkII <c2h5oc2h4@outlook.com>
2025-04-11 13:29:02 +0800

    update

--- mod/linux-home-desktop/niri.nix
+++ mod/linux-home-desktop/niri.nix
@@ -1,5 +1,13 @@
 { config, lib, pkgs, cfg, ... }:
 {
+  xdg.configFile."fuzzel" = {
+    source = cfg.lib.relativeToRoot "dot/${cfg.sys.config}/fuzzel";
+    recursive = true;
+  };
+  xdg.configFile."waybar" = {
+    source = cfg.lib.relativeToRoot "dot/${cfg.sys.config}/waybar";
+    recursive = true;
+  };
   xdg.configFile."niri" = {
     source = cfg.lib.relativeToRoot "dot/${cfg.sys.config}/niri";
     recursive = true;
