27936ad550b27632efc1f2bd879532772b191799
FrdrCkII <c2h5oc2h4@outlook.com>
2025-04-12 22:43:19 +0800

    update

--- mod/linux-home-programs/nixGL.nix
+++ mod/linux-home-programs/nixGL.nix
@@ -1,7 +1,8 @@
 { config, lib, pkgs, cfg, ... }:
 {
-  nixGL = {
-    enable = true;
-    packages = cfg.pkg.nixGL.package;
-  };
+  # nixGL = {
+  #   enable = true;
+  #   packages = cfg.pkg.nixGL.package;
+  # };
+  home.packages = cfg.pkg.nixGL.auto.nixGL;
 }
