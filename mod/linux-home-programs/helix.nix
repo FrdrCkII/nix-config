dddd33ed6c0a3cc5cf2b30563f5cbf53c0a98605
FrdrCkII <c2h5oc2h4@outlook.com>
2025-04-12 21:26:37 +0800

    update

--- mod/linux-home-programs/helix.nix
+++ mod/linux-home-programs/helix.nix
@@ -3,14 +3,9 @@
   programs.helix = {
     enable = true;
     settings = {
-      theme = snazzy;
+      theme = "snazzy";
       editor = {
-        rulers = [72,80];
-        cursor-shape = {
-          normal = "block";
-          insert = "bar";
-          select = "underline";
-        };
+        rulers = ["72" "80"];
       };
     };
     languages.language = [{
