b12674baa32b0726701bc8215f7d819ac1f35f5b
FrdrCkII <c2h5oc2h4@outlook.com>
2025-04-19 10:39:30 +0800

    update

--- mod/linux-home-programs/rustup.nix
+++ mod/linux-home-programs/rustup.nix
@@ -3,14 +3,14 @@
   home.packages = with pkgs; [
     rustup
   ];
-  programs.zsh.initExtra =  = lib.mkMerge [
+  programs.zsh.initExtra = lib.mkMerge [
     (''
       export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
       export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
       export PATH=$PATH:${config.home.homeDirectory}/.cargo/bin
     '')
   ];
-  programs.bash.initExtra =  = lib.mkMerge [
+  programs.bash.initExtra = lib.mkMerge [
     (''
       export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
       export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
